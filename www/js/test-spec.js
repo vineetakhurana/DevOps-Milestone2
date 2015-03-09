d848a880ca80fa23e5b2d9c4a226f8a619c923e8
vineetakhurana <vkhuran2@ncsu.edu>
2015-03-08 20:05:34 -0400

    changed tests

--- www/js/test-spec.js
+++ www/js/test-spec.js
@@ -6,20 +6,20 @@
 //var z = require('jasmine-check');
 
 describe("Answer 1", function() {
-  // it("computes no Answer", function() {
-  //   //fuzzer = require('fuzzer');
-  //   var target = new SiteSettingsViewModel();
-  //   //target.Pref = "true"
-  //   expect(target.Pref1()).toBe("Not yet attempted");
-  // });
+  it("computes no Answer", function() {
+    //fuzzer = require('fuzzer');
+    var target = new SiteSettingsViewModel();
+    //target.Pref = "true"
+    expect(target.Pref1()).toBe("Not yet attempted");
+  });
 
-// it("gives wrong input - for correct ans", function() {
-//     var target = new SiteSettingsViewModel();
+it("gives wrong input - for correct ans", function() {
+    var target = new SiteSettingsViewModel();
 
-//     target.Pref("false");
-//     //ko.utils.unwrapObservable(self.Pref)
-//     expect(target.Pref1()).toBe("Correct!!");
-//   });
+    target.Pref("false");
+    //ko.utils.unwrapObservable(self.Pref)
+    expect(target.Pref1()).toBe("Correct!!");
+  });
 
 
    it("computes correct Answer", function() {
@@ -29,13 +29,12 @@ describe("Answer 1", function() {
     expect(target.Pref1()).toBe("Correct!!");
   });
 
-/*
-  //  it("computes wrong Answer", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.Pref("false");
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.Pref1()).toBe("Incorrect!!");
-  // });
+   it("computes wrong Answer", function() {
+    var target = new SiteSettingsViewModel();
+    target.Pref("false");
+    //ko.utils.unwrapObservable(self.Pref)
+    expect(target.Pref1()).toBe("Incorrect!!");
+  });
 
    
     it("gives wrong input - for incorrect ans", function() {
@@ -45,12 +44,12 @@ describe("Answer 1", function() {
     expect(target.Pref1()).toBe("Incorrect!!");
   });
 
-  //   it("gives wrong input - integer", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.q3(8879);
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.Pref1()).toBe("Correct!!");
-  // });
+    it("gives wrong input - integer", function() {
+    var target = new SiteSettingsViewModel();
+    target.q3(8879);
+    //ko.utils.unwrapObservable(self.Pref)
+    expect(target.Pref1()).toBe("Correct!!");
+  });
 
     it("gives wrong input - float", function() {
     var target = new SiteSettingsViewModel();
@@ -58,126 +57,6 @@ describe("Answer 1", function() {
     //ko.utils.unwrapObservable(self.Pref)
     expect(target.Pref1()).toBe("Incorrect!!");
   });
-*/
 
-});
-
-
-describe("Answer 2", function() {
-  it("computes no Answer", function() {
-    var target = new SiteSettingsViewModel();
-    //target.Pref = "true"
-    expect(target.answer2()).toBe(" Not yet attempted");
-  });
-
-  /*
-
-   it("computes correct Answer", function() {
-    var target = new SiteSettingsViewModel();
-    target.q2("false");
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer2()).toBe("Correct!!");
-  });
-
-  //    it("computes wrong Answer", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.q2("true");
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.answer2()).toBe("Incorrect!!");
-  // });
-
-   it("gives wrong input - for correct ans", function() {
-    var target = new SiteSettingsViewModel();
-    target.q2("true");
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer2()).toBe("Correct!!");
-  });
-
-  //   it("gives wrong input - for incorrect ans", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.q2("false");
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.answer2()).toBe("Incorrect!!");
-  // });
-
-    it("gives wrong input - integer", function() {
-    var target = new SiteSettingsViewModel();
-    target.q2(410);
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer2()).toBe("Correct!!");
-  });
-
-    it("gives wrong input - float", function() {
-    var target = new SiteSettingsViewModel();
-    target.q3(12.69);
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer3()).toBe("Incorrect!!");
-  });   
-*/
-
-});
-
-describe("Answer 3", function() {
-  // it("computes no Answer", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   //target.Pref = "true"
-  //   expect(target.answer3()).toBe(" Not yet attempted");
-  // });
-
-  //  it("computes correct Answer", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.q3("true");
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.answer3()).toBe("Correct!!");
-  // });
-
-    it("computes wrong Answer", function() {
-    var target = new SiteSettingsViewModel();
-    target.q3("false");
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer3()).toBe("Incorrect!!");
-  });
-/*
-
-     it("gives wrong input - for correct ans", function() {
-    var target = new SiteSettingsViewModel();
-    target.q3("false");
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer3()).toBe("Correct!!");
-  });
-
-  //   it("gives wrong input - for incorrect ans", function() {
-  //   var target = new SiteSettingsViewModel();
-  //   target.q3("true");
-  //   //ko.utils.unwrapObservable(self.Pref)
-  //   expect(target.answer3()).toBe("Incorrect!!");
-  // });
-
-    it("gives wrong input - integer", function() {
-    var target = new SiteSettingsViewModel();
-    target.q3(8879);
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer3()).toBe("Correct!!");
-  });
-
-    it("gives wrong input - float", function() {
-    var target = new SiteSettingsViewModel();
-    target.q3(12.69);
-    //ko.utils.unwrapObservable(self.Pref)
-    expect(target.answer3()).toBe("Incorrect!!");
-  });
-
-
-// describe("tester", function() {
-//   z.check.it("tests random",[gen.string], function(x) {
-//     //var target = new SiteSettingsViewModel();
-//     //target.Pref = "true"
-//     expect(x).toEqual(jasmine.any(String));
-
-
-//   });
-// });  
-
-*/
 
 });
